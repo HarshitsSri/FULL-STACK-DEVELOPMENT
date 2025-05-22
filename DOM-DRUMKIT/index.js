33a33130c51b6a9676cda32cc6634268d5eee511
HarshitsSri <srivastavh757@gmail.com>
2025-05-22 15:22:26 +0530

    Now working on a new project with event listner audio class etc

---
+++ DOM-DRUMKIT/index.js
@@ -0,0 +1,8 @@
+
+    for(var i=0;i<document.querySelectorAll(".drum").length;i++){
+        document.querySelectorAll(".drum")[i].addEventListener("click",handleclick);
+    }
+    //document.querySelector(".drum").addEventListener("click",handleclick);
+    function handleclick(){
+        alert("i got clicked");
+    }
